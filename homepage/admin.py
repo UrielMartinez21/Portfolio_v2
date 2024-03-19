e26b75d25b361f1839327bb75558675fcfa0b991
UrielMartinez21 <uriellmartinezz35@gmail.com>
2024-03-18 18:21:07 -0600

    work in about me

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from homepage.models import Activity
+from homepage.models import Activity, Description
 
 
 # Register your models here.
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ['title', 'created', 'updated']
+    search_fields = ['title', 'description']
+    ordering = ['created']
+
+
+@admin.register(Description)
+class DescripcionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type', 'created', 'updated',]
     search_fields = ['title', 'description']
     ordering = ['created']
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from homepage.models import Activity
+from homepage.models import Activity, Description
 from projects.models import Project
 
 
@@ -11,7 +11,23 @@ def home(request):
 
 
 def about_me(request):
-    return render(request, 'about_me.html')
+    # Get information per type
+    personal = Description.objects.filter(type='personal')
+    education = Description.objects.filter(type='education')
+    jobs = Description.objects.filter(type='jobs')
+    skills = Description.objects.filter(type='skills')
+    career = Description.objects.filter(type='career')
+
+    # Create a dictionary with all the information
+    info = {
+        'educations': education,
+        'jobs': jobs,
+        'careers': career,
+        'skills': skills,
+        'personals': personal,
+    }
+
+    return render(request, 'about_me.html', info)
 
 
 def projects(request):
